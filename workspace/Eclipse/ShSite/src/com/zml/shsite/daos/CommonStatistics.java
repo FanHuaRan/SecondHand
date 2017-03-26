c7250734111ada286233fcde6f0f9ea3c41c6a0f
FanHuaRan <834171100@qq.com>
2017-03-26 19:13:08 +0800

    2017/03/26 fhr

--- workspace/Eclipse/ShSite/src/com/zml/shsite/daos/CommonStatistics.java
+++ workspace/Eclipse/ShSite/src/com/zml/shsite/daos/CommonStatistics.java
@@ -4,13 +4,19 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class CommonStatistics extends HibernateBaseDao {
+public class CommonStatistics extends HibernateDaoSupport {
 	
 	private static final Logger logger=Logger.getLogger(CommonStatistics.class);
-	
+	@Autowired
+	public CommonStatistics(SessionFactory sessionFactory){
+		this.setSessionFactory(sessionFactory);
+	}
 	public Object statisUniqueResult(String hql,List<Object> params){
 		try {
 			Query query = this.getSession().createQuery(hql);
--- workspace/Eclipse/ShSite/src/com/zml/shsite/services/impl/CreateGoodViewModelImpl.java
+++ workspace/Eclipse/ShSite/src/com/zml/shsite/services/impl/CreateGoodViewModelImpl.java
@@ -2,14 +2,17 @@ package com.zml.shsite.services.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.zml.shsite.daos.CommonStatistics;
 import com.zml.shsite.models.Good;
 import com.zml.shsite.services.ICreateGoodViewModel;
 import com.zml.shsite.viewmodels.GoodViewModel;
 @Service
-public class CreateGoodViewModelImpl implements ICreateGoodViewModel{
-
+public class CreateGoodViewModelImpl implements ICreateGoodViewModel {
+	@Autowired
+	private CommonStatistics commonStatistics=null;
 	@Override
 	public GoodViewModel create(Good good) {
 		// TODO Auto-generated method stub
